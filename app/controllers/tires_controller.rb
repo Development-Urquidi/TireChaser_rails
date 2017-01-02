d3354b3d6d894dda67c43c1b53c433c0263fa1ac
Development-Urquidi <pro.urquidi@gmail.com>
2017-01-02 16:42:43 -0600

    Reversion edit

---
+++ app/controllers/tires_controller.rb
@@ -0,0 +1,46 @@
+class TiresController < ApplicationController
+  def new
+    @tire = Tire.new
+  end
+
+  def create
+    @vendor = Vendor.find(params[:vendor_id])
+    @tire = @vendor.tires.create(tire_params)
+      redirect_to vendor_path(@vendor)
+
+  end
+
+  def edit
+    @vendor = Vendor.find(params[:vendor_id])
+    @tire = @vendor.tires.find(params[:id])
+  end
+
+  def update
+    @vendor = Vendor.find(params[:vendor_id])
+    @tire = @vendor.tires.find(params[:id])
+
+    if @tire.update(tires_params)
+      redirect_to admin_vendors_path(@vendor)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @vendor = Vendor.find(params[:vendor_id])
+    @tire = @vendor.tires.find(params[:id])
+    @tire.destroy
+    redirect_to vendor_path(@vendor)
+  end
+
+  def show
+    @tire = Tire.find(params[:id])
+  end
+
+
+  private
+  def tire_params
+    params.require(:tire).permit(:model, :brand, :price, :quantity)
+  end
+
+end
